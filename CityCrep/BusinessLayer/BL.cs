1a23cfdc4575507dce7dd158d6a50ab691439218
Mohamed Fawzy <mohamed.fawzy9876@gmail.com>
2017-04-04 11:35:46 -0700

    Add project files.

---
+++ CityCrep/BusinessLayer/BL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace CityCrep.BusinessLayer
+{
+    class BL
+    {
+        DataAccessLayer.DAL D;
+        public BL()
+        {
+
+        }
+
+        // Get User for login
+        public DataTable get_user(string u_name, string pass)
+        {
+            D = new DataAccessLayer.DAL();
+
+            if (D.getError()) return null;
+
+            DataTable dt = new DataTable();
+            MySqlParameter[] param = new MySqlParameter[2];
+            param[0] = new MySqlParameter("u_name", MySqlDbType.VarChar);
+            param[0].Value = u_name;
+            param[1] = new MySqlParameter("pass", MySqlDbType.VarChar);
+            param[1].Value = pass;
+            dt = D.SelectData("get_user", param);
+            return dt;
+        }
+
+        // Add new Product
+        public void add_product(string name, float price, int size, string category)
+        {
+            D = new DataAccessLayer.DAL();
+            if (D.getError()) return;
+
+            MySqlParameter[] param = new MySqlParameter[4];
+            param[0] = new MySqlParameter("pro_name", MySqlDbType.VarChar);
+            param[0].Value = name;
+            param[1] = new MySqlParameter("price", MySqlDbType.Float);
+            param[1].Value = price;
+            param[2] = new MySqlParameter("size", MySqlDbType.Int32);
+            param[2].Value = size;
+            param[3] = new MySqlParameter("category", MySqlDbType.VarChar);
+            param[3].Value = category;
+            D.ExecuteCommand("add_product", param);
+        }
+
+
+
+
+    }
+}
---
+++ CityCrep/DataAccessLayer/DAL.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace CityCrep.DataAccessLayer
+{
+    class DAL
+    {
+        MySqlConnection MySqlCon;
+        private bool error = false;
+
+        public bool getError()
+        {
+            return error;
+        }
+
+        public DAL()
+        {
+            Connect();
+            
+        }
+
+        public void Connect()
+        {
+
+            String server = "107.180.24.243";
+            String database = "SmartBasket";
+            String user = "samehkaras";
+            String password = "Jesuslove1";
+            String mode = "online";
+            String port = "3306";
+            if (mode == "online")
+            {
+               // MySqlCon = new MySqlConnection("SERVER=" + server + ";PORT=" + port + ";DATABASE=" + database + ";UID=" + user + ";PASSWORD=" + password + "default command timeout = 200;");
+               // MySqlCon = new MySqlConnection("SERVER=107.180.24.243;PORT=3306;DATABASE=SmartBasket;UID=samehkaras;PASSWORD=Jesuslove1;charset=utf8;Allow Zero Datetime=True; default command timeout = 200;");
+            }
+            else
+            {
+               MySqlCon= new MySqlConnection("SERVER=" + server + ";DATABASE=" + database + ";UID=" + user + ";PASSWORD=" + password);
+
+            }
+            Open();
+        }
+
+        public void Open()
+        {
+            if (MySqlCon.State != ConnectionState.Open)
+            {
+                try
+                {
+                    MySqlCon.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("Error in the Internet Connnection!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    error = true;
+                }
+            }
+        }
+
+        public void Close()
+        {
+
+            if (MySqlCon.State == ConnectionState.Open)
+            {
+                try
+                {
+                    MySqlCon.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Error in the Internet Connnection!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    error = true;
+                }
+            }
+
+        }
+
+        public DataTable SelectData(string stored_procedure, MySqlParameter[] param)
+        {
+
+            DataTable dt = new DataTable();
+            MySqlCommand MySqlcmd = new MySqlCommand();
+            MySqlcmd.CommandType = CommandType.StoredProcedure;
+            MySqlcmd.CommandText = stored_procedure;
+
+            try
+            {
+                this.Open();
+                MySqlcmd.Connection = MySqlCon;
+
+                if (param != null)
+                    MySqlcmd.Parameters.AddRange(param);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(MySqlcmd);
+                da.Fill(dt);
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error in the Internet Connnection!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                error = true;
+            }
+            return dt;
+        }
+        public void ExecuteCommand(string stored_procedure, MySqlParameter[] param)
+        {
+            MySqlCommand MySqlcmd = new MySqlCommand();
+            MySqlcmd.CommandType = CommandType.StoredProcedure;
+            MySqlcmd.CommandText = stored_procedure;
+            try
+            {
+                this.Open();
+                MySqlcmd.Connection = MySqlCon;
+                if (param != null)
+                    MySqlcmd.Parameters.AddRange(param);
+                MySqlcmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error in the Internet Connnection!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                error = true;
+            }
+        }
+
+    }
+}
